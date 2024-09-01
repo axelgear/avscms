56b42e597885420acadecd612e7960096994bf18
Axel <43678396+axelgear@users.noreply.github.com>
2024-09-01 14:37:18 +0530

    Update categories.php

--- categories.php
+++ categories.php
@@ -2,6 +2,7 @@
 define('_VALID', true);
 require 'include/config.php';
 require 'include/function_smarty.php';
+require 'classes/pagination.class.php';
 
 $s      = ( isset($_GET['s']) && ($_GET['s'] == 'a' or $_GET['s'] == 'g') ) ? $_GET['s'] : '';
 
@@ -31,7 +32,12 @@ if ($s == "a") {
 	}
 	
 } else {
-	$sql            = "SELECT CHID, name, slug FROM channel ORDER BY name ASC";
+	$sql            = "SELECT count(CHID) AS total_categories FROM channel";
+	$rsc            = $conn->execute($sql);
+	$total          = $rsc->fields['total_categories'];
+	$pagination     = new Pagination($config['categories_per_page']);
+	$limit          = $pagination->getLimit($total);
+	$sql            = "SELECT CHID, name, slug FROM channel ORDER BY name ASC LIMIT " . $limit;
 	$rs             = $conn->execute($sql);
 	$categories     = $rs->getrows();
 
@@ -59,14 +65,22 @@ if ($s == "a") {
 if ($s == "a") {
 	$smarty->assign('section', "a");
 } else {
+	$page_link      = $pagination->getPagination('categories');
+	$smarty->assign('base', 'categories');
 	$smarty->assign('section', "v");
 }
 
+$start_num      = $pagination->getStartItem();
+$end_num        = $pagination->getEndItem();
 $smarty->assign('errors',$errors);
 $smarty->assign('messages',$messages);
 $smarty->assign('menu', 'categories');
 $smarty->assign('catgy', true);
 $smarty->assign('categories', $categories);
+$smarty->assign('categories_total', $total);
+$smarty->assign('start_num', $start_num);
+$smarty->assign('end_num', $end_num);
+$smarty->assign('page_link', $page_link);
 $smarty->assign('self_title', $seo['categories_title']);
 $smarty->assign('self_description', $seo['categories_desc']);
 $smarty->assign('self_keywords', $seo['categories_keywords']);
