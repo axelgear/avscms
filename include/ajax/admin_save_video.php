9deb87df495b1f3622e7eeebd80c3994c5163684
Axel <43678396+axelgear@users.noreply.github.com>
2024-09-02 20:50:43 +0530

    Update admin_save_video.php

--- include/ajax/admin_save_video.php
+++ include/ajax/admin_save_video.php
@@ -22,7 +22,7 @@ $vid            = trim($data['id']);
 $title          = trim($data['title']);
 $description    = trim($data['description']);
 $keyword        = prepare_tags(trim($data['tags']));
-$channel        = trim($data['category']);
+//$channel        = trim($data['category']);
 $type           = trim($data['type']);
 $featured       = trim($data['featured']);
 $be_comment     = trim($data['be_comment']);
@@ -33,7 +33,16 @@ $dislikes       = trim($data['dislikes']);
 $viewnumber     = trim($data['viewnumber']);
 $active         = trim($data['active']);
 $server			= trim($data['server']);
-
+// Set default channel values
+$channel = $channel2 = $channel3 = $channel4 = $channel5 = $channel6 = $channel7 = $channel8 = null;
+// Assign category values to the correct variables
+if (isset($data['category']) && is_array($data['category'])) {
+    if (isset($data['category'][0])) $channel = trim($data['category'][0]);
+    if (isset($data['category'][1])) $channel2 = trim($data['category'][1]);
+    if (isset($data['category'][2])) $channel3 = trim($data['category'][2]);
+    if (isset($data['category'][3])) $channel4 = trim($data['category'][3]);
+    if (isset($data['category'][4])) $channel5 = trim($data['category'][4]);
+}
 settype($vid, 'integer');
 settype($viewnumber, 'integer');
 settype($likes, 'integer');
@@ -47,6 +56,8 @@ else
 update_tags($vid, $keyword);
 $sql = "UPDATE video SET title = " .$conn->qStr($title). ", description = " .nl2br2($conn->qStr($description)). ",
 						 keyword = " .$conn->qStr($keyword). ", channel = " .$channel. ", type = " .$conn->qStr($type). ",
+       						 channel2 = " . $conn->qStr($channel2) . ", channel3 = " . $conn->qStr($channel3) . ", 
+						 channel4 = " . $conn->qStr($channel4) . ",
 						 featured = " .$conn->qStr($featured). ", be_comment = " .$conn->qStr($be_comment). ",
 						 be_rated = " .$conn->qStr($be_rated). ", embed = " .$conn->qStr($embed). ",
 						 likes = " .$conn->qStr($likes). ", dislikes = " .$conn->qStr($dislikes). ", 
